7a2e5ca7b9c53e861a01918a8db91a808eef3651
Wirote Kaewroung <shellclub@gmail.com>
2024-10-27 11:32:40 +0700

    update

--- src/app/(main-website)/page.tsx
+++ src/app/(main-website)/page.tsx
@@ -1,10 +1,19 @@
 import { Container } from "@mui/material";
 import AppLogo from "./components/AppLogo";
 
-export default function Home(){
+async function getData(){
+  const response = await fetch('https://dummyjson.com/users')
+
+  if(!response.ok){
+        throw new Error("ไม่สามารถ Fetch Data ได้")
+  }
+  return response.json()
+}
+export default async function Home(){
+  const response  = await getData()
   return(
     <Container>
-      <h1>Home page</h1>
+      <h1>{JSON.stringify(response.users)}</h1>
       <AppLogo title="Logo One" colors="green"/>
       <AppLogo title="Logo Two" colors="blue"/>
     </Container>
